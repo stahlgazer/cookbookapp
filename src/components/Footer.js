4e3509eaa3abafa8e41e44f25528d4629d12ff64
Gavin S <stahlgavin@gmail.com>
2022-06-13 23:11:56 -0700

    some basic styling for nav/footer to have something to work with

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Footer = () => {
-  return <footer><h3>footer content</h3></footer>;
+  return (
+    <footer>
+      <h5>Copyright &copy; 2022 DigitalCookbook. All rights Reserved.</h5>
+    </footer>
+  );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/recipes">Recipes</Link>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/contact">Contact</Link>
+      <div className="navitems">
+        <Link to="/">Home</Link>
+        <Link to="/recipes">Recipes</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/contact">Contact</Link>
+      </div>{" "}
+      <Avatar
+        className="avatar"
+        alt="User Avatar"
+        src="/static/images/avatar/1.jpg"
+      />
     </nav>
   );
 };
