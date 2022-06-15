b8c987445640f7f3d5fc19791d0338c8066661d9
Gavin S <stahlgavin@gmail.com>
2022-06-15 14:44:50 -0700

    added search/filtering for recipes

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 export const RecipeList = () => {
   const [recipes, setRecipes] = useState();
+  const [searchCategory, setSearchCategory] = useState("");
 
   useEffect(() => {
     axios
@@ -15,19 +16,33 @@ export const RecipeList = () => {
 
   return (
     <section>
+      <button onClick={() => setSearchCategory("")}>All</button>
+      <button onClick={() => setSearchCategory("breakfast")}>Breakfast</button>
+      <button onClick={() => setSearchCategory("lunch")}>Lunch</button>
+      <button onClick={() => setSearchCategory("dinner")}>Dinner</button>
+      <button onClick={() => setSearchCategory("dessert")}>Dessert</button>
       {recipes &&
-        recipes.map((recipe) => {
-          return (
-            <div key={recipe.id}>
-              <p>name: {recipe.name}</p>
-              <p>author: {recipe.author}</p>
-              <p>info: {recipe.description}</p>
-              <p>{recipe.image}</p>
-              <p>{recipe.category}</p>
-              <Link to="/">Back Home</Link>
-            </div>
-          );
-        })}
+        recipes
+          .filter((values) => {
+            if (searchCategory === "") {
+              return values;
+            }
+            if (values.category.includes(searchCategory)) {
+              return values;
+            } else return false;
+          })
+          .map((recipe) => {
+            return (
+              <div key={recipe.id}>
+                <p>name: {recipe.name}</p>
+                <p>author: {recipe.author}</p>
+                <p>info: {recipe.description}</p>
+                <p>{recipe.image}</p>
+                <p>{recipe.category}</p>
+                <Link to="/">Back Home</Link>
+              </div>
+            );
+          })}
     </section>
   );
 };
